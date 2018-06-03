ca81388ea9b913ed7444a59c0c84f68d626912e4
99cm <zyizyao@china-guide.com>
2018-06-03 17:19:14 -0400

    Add update

--- app/controllers/spree/admin/slides_controller.rb
+++ app/controllers/spree/admin/slides_controller.rb
@@ -4,7 +4,31 @@ module Spree
       respond_to :html
 
       def index
-        @slides = Spree::Slide.order(:position)
+        @slides = Spree::Slide.order(:position).page(params[:page]).per(params[:per_page])
+      end
+
+      def update
+        invoke_callbacks(:update, :before)
+        if @object.update_attributes(permitted_resource_params)
+          invoke_callbacks(:update, :after)
+          handle_slide_locations(@object)
+          respond_with(@object) do |format|
+            format.html do
+              flash[:success] = flash_message_for(@object, :successfully_updated)
+              redirect_to location_after_save
+            end
+            format.js { render layout: false }
+          end
+        else
+          invoke_callbacks(:update, :fails)
+          respond_with(@object) do |format|
+            format.html do
+              flash.now[:error] = @object.errors.full_messages.join(", ")
+              render_after_update_error
+            end
+            format.js { render layout: false }
+          end
+        end
       end
 
       private
@@ -17,8 +41,16 @@ module Spree
         end
       end
 
-      def slide_params
-        params.require(:slide).permit(:name, :body, :link_url, :published, :image, :position, :product_id)
+      def permitted_resource_params
+        params.require(:slide).permit(:name, :body, :link_url, :published, :image, :position, :product_id, :slide_location_ids)
+      end
+
+      def handle_slide_locations(object)
+        slide_location_ids = permitted_resource_params[:slide_location_ids].split(',')
+
+        slide_location_ids.each do |slide_location_id|
+          object.slide_slide_locations.find_or_create_by(slide_id: object.id, slide_location_id: slide_location_id)
+        end
       end
     end
   end
